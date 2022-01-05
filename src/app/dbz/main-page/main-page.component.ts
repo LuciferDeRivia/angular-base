cf05baaf58334dcc46c5834b2869e939683f476a
claudio_hernandez <chernandez@hostyweb.cl>
2022-01-05 15:30:28 -0300

    Cambiar inputs con ngModel

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -11,20 +11,32 @@ interface Personaje {
   styleUrls: ['./main-page.component.css'],
 })
 export class MainPageComponent {
+  personajes: Personaje[] = [
+    {
+      nombre: 'Goku',
+      poder: 15000,
+    },
+    {
+      nombre: 'Vegeta',
+      poder: 7500,
+    },
+  ];
+
   nuevo: Personaje = {
-    nombre: 'Trucks',
-    poder: 14000,
+    nombre: '',
+    poder: 0,
   };
 
   agregar() {
-    console.log(this.nuevo);
+    if (this.nuevo.nombre.trim().length === 0) {
+      return;
+    }
+
+    this.personajes.push(this.nuevo);
+    this.nuevo = { nombre: '', poder: 0 };
   }
 
-  cambiarNombre(event: any) {
-    console.log(event);
-  }
-
-  cambiarEdad(event: any) {
-    console.log(event);
-  }
+  // cambiarNombre(event: any) {
+  //   console.log(event);
+  // }
 }
