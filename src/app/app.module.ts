e54b15f211809302197d1ab3743752c9300a88b8
claudio_hernandez <chernandez@hostyweb.cl>
2022-01-05 15:00:38 -0300

    Prevenir el evento de formulario manualmente con

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeroesModule } from './heroes/heroes.module';
 import { ContadorModule } from './contador/contador.module';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
   // componentes que se estan utilizando
   declarations: [AppComponent],
   // otros modulos de la aplicación
-  imports: [BrowserModule, HeroesModule, ContadorModule],
+  imports: [BrowserModule, HeroesModule, ContadorModule, DbzModule],
   // servicios especificos a un modulo
   providers: [],
   // modulo inicial
