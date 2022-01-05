8348d4ce226dc8211c4625e6f89d1232139eb6ff
claudio_hernandez <chernandez@hostyweb.cl>
2022-01-05 15:14:01 -0300

    Cambiar inputs con handleFunctions

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,12 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Personaje {
+  nombre: string;
+  poder: number;
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css'],
 })
 export class MainPageComponent {
+  nuevo: Personaje = {
+    nombre: 'Trucks',
+    poder: 14000,
+  };
+
   agregar() {
-    console.log('prueba');
+    console.log(this.nuevo);
+  }
+
+  cambiarNombre(event: any) {
+    console.log(event);
+  }
+
+  cambiarEdad(event: any) {
+    console.log(event);
   }
 }
